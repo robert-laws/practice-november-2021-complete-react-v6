c3bf537bdbbf3bab436f6fb17f937d1a1fce32a1
Robert Laws <roblaws@me.com>
2021-11-27 13:07:55 +0300

    react without build process

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pet = (props) => {
+  return React.createElement('div', {}, [
+    React.createElement('h2', {}, props.name),
+    React.createElement('h3', {}, props.animal),
+    React.createElement('h3', {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt Me'),
+    React.createElement(Pet, {
+      name: 'Luna',
+      animal: 'Dog',
+      breed: 'Poodle',
+    }),
+    React.createElement(Pet, {
+      name: 'Spot',
+      animal: 'Dog',
+      breed: 'Golden Retriever',
+    }),
+    React.createElement(Pet, {
+      name: 'Felix',
+      animal: 'Cat',
+      breed: 'Tabby',
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
